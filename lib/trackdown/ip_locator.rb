d5ebb87f2171519cf61c35d2649d46223d345046
Javi R <4920956+rameerez@users.noreply.github.com>
2024-09-26 14:49:58 +0300

    Make `trackdown` return a Ruby object instead of a hash so we can call methods like `.emoji`

--- lib/trackdown/ip_locator.rb
+++ lib/trackdown/ip_locator.rb
@@ -1,24 +1,48 @@
 require 'maxmind/db'
 
 module Trackdown
+  class LocationResult
+    attr_reader :country_code, :city, :emoji_flag
+
+    def initialize(country_code, city, emoji_flag)
+      @country_code = country_code
+      @city = city
+      @emoji_flag = emoji_flag
+    end
+
+    def emoji
+      @emoji_flag
+    end
+
+    def to_h
+      {
+        country_code: @country_code,
+        city: @city,
+        emoji_flag: @emoji_flag
+      }
+    end
+  end
+
   class IpLocator
     class << self
       def locate(ip)
         record = fetch_record(ip)
-        {
-          country_code: extract_country_code(record),
-          city: extract_city(record),
-          emoji_flag: get_emoji_flag(extract_country_code(record))
-        }
+        country_code = extract_country_code(record)
+        city = extract_city(record)
+        emoji_flag = get_emoji_flag(country_code)
+        LocationResult.new(country_code, city, emoji_flag)
       end
 
       private
 
       def fetch_record(ip)
+        Trackdown.ensure_database_exists!
         reader = MaxMind::DB.new(Trackdown.configuration.database_path, mode: MaxMind::DB::MODE_MEMORY)
         record = reader.get(ip)
         reader.close
         record
+      rescue Trackdown::Error => e
+        raise e
       rescue => e
         Rails.logger.error("Error fetching IP data: #{e.message}") if defined?(Rails)
         nil
