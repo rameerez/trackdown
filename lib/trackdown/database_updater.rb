12edad06ea26bc842b8559d9eafd389460feb7b5
Javi R <4920956+rameerez@users.noreply.github.com>
2024-10-29 01:20:43 +0000

    Make the database downloader actually work with MaxMind credentials

--- lib/trackdown/database_updater.rb
+++ lib/trackdown/database_updater.rb
@@ -5,18 +5,26 @@ require 'rubygems/package'
 module Trackdown
   class DatabaseUpdater
     DOWNLOAD_URL = "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=%{license_key}&suffix=tar.gz"
-    # DOWNLOAD_URL = "http://localhost:3002/GeoLite2-City.mmdb.tar.gz"
 
     class << self
       def update
         download_url = DOWNLOAD_URL % { license_key: Trackdown.configuration.maxmind_license_key }
 
-        URI.open(download_url,
-                 http_basic_authentication: [Trackdown.configuration.maxmind_account_id, Trackdown.configuration.maxmind_license_key]) do |remote_file|
+        options = {
+          http_basic_authentication: [
+            Trackdown.configuration.maxmind_account_id.to_s,
+            Trackdown.configuration.maxmind_license_key.to_s
+          ],
+          ssl_verify_mode: OpenSSL::SSL::VERIFY_PEER
+        }
+
+        URI.open(download_url, **options) do |remote_file|
           Zlib::GzipReader.wrap(remote_file) do |gz|
             Gem::Package::TarReader.new(gz) do |tar|
               tar.each do |entry|
                 if entry.full_name.end_with?('.mmdb')
+                  FileUtils.mkdir_p(File.dirname(Trackdown.configuration.database_path))
+
                   File.open(Trackdown.configuration.database_path, 'wb') do |file|
                     file.write(entry.read)
                   end
@@ -28,8 +36,21 @@ module Trackdown
         end
 
         Rails.logger.info("MaxMind database updated successfully") if defined?(Rails)
+        true
+      rescue OpenURI::HTTPError => e
+        message = case e.message
+        when /401/
+          "Authentication failed. Please check your MaxMind account ID and license key."
+        when /403/
+          "Access forbidden. Your MaxMind license may not have access to this database."
+        else
+          "HTTP Error: #{e.message}"
+        end
+        Rails.logger.error("Error updating MaxMind database: #{message}") if defined?(Rails)
+        raise Error, message
       rescue => e
         Rails.logger.error("Error updating MaxMind database: #{e.message}") if defined?(Rails)
+        raise Error, "Failed to update database: #{e.message}"
       end
     end
   end
